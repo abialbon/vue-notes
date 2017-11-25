0ac3f21bd96645d3fbf5f3646d896fc1dad26b04
Abialbon Paul <dr.abialbon@gmail.com>
2017-11-25 18:10:13 -0400

    Add basic components

---
+++ src/components/AppAside.vue
@@ -0,0 +1,17 @@
+<template>
+  <aside>
+    Aside
+  </aside>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  aside {
+    background-color: #2b2b2b;
+    width: 100px;
+    height: 100vh;
+  }
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    Main notes area
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+  section {
+    padding: 40px;
+    background-color: rgba(181,203,3,0.11);
+    flex: 1;
+  }
+</style>
